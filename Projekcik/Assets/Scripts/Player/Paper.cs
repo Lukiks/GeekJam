42e3657b52ff661b0a407f1c2323133aaf318ede
Lukiks <lukaszkrywult02@gmail.com>
2021-06-01 21:30:29 +0200

    Player Scripts and interations

---
+++ Projekcik/Assets/Scripts/Player/Paper.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paper : MonoBehaviour
+{
+
+    public bool[] papers;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
