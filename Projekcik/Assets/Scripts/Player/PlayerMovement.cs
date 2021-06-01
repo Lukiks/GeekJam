9cd6e397a08f65cb3600fa48ce14c14971cffd73
Lukiks <lukaszkrywult02@gmail.com>
2021-06-01 14:20:19 +0200

    First Push Unity Prodject (player movement and camera movement)

---
+++ Projekcik/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
