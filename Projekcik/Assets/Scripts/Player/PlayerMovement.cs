e8c6ca55f9a865a9b809d623b3329e74e3b0f6a5
Lukiks <lukaszkrywult02@gmail.com>
2021-06-02 05:32:22 +0200

    Adding animations and more sprites

--- Projekcik/Assets/Scripts/Player/PlayerMovement.cs
+++ Projekcik/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,11 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator anim;
 
     void Update()
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
